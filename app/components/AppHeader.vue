3328d91b0140d5a21f6ab85168da9775a8a27da2
a.amerkanova <assel.amerkanova@digitaleconomy.kz>
2025-10-16 11:28:14 +0500

    -extra files

--- app/components/AppHeader.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <header class="p-4 bg-gray-800 text-white flex justify-between">
-    <NuxtLink to="/">Главная</NuxtLink>
-    <nav class="flex gap-3">
-      <NuxtLink to="/login">Логин</NuxtLink>
-    </nav>
-  </header>
-</template>
